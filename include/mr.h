52dcb24fd63faff548ea55461be015a3f7ce4088
Enrico <enrico.melis.casa@gmail.com>
2026-05-27 16:27:35 +0000

    .

--- include/mr.h
+++ include/mr.h
@@ -15,9 +15,9 @@ typedef struct mr *mr_t;
  * log_file       : nome del file di log, oppure NULL per usare il nome di default.
  */
 typedef struct {
-    size_t      mapper_threads;
-    size_t      reducer_threads;
-    size_t      queue_size;
+    size_t mapper_threads;
+    size_t reducer_threads;
+    size_t queue_size;
     const char *log_file;
 } mr_attr_t;
 
@@ -25,11 +25,11 @@ typedef struct {
  * Riga logica di un file, vista dalla funzione mapper.
  */
 typedef struct {
-    const char   *file_name;
-    size_t        file_name_len;
+    const char *file_name;
+    size_t file_name_len;
     unsigned long line_number;
-    const char   *line;
-    size_t        line_len;
+    const char *line;
+    size_t line_len;
 } mr_file_line_t;
 
 /*
@@ -39,7 +39,7 @@ typedef struct {
  */
 typedef struct {
     const void *data;
-    size_t      size;
+    size_t size;
 } mr_value_t;
 
 /*
@@ -47,44 +47,26 @@ typedef struct {
  * token deve essere una stringa C valida, terminata da '\0',
  * composta soltanto da caratteri alfanumerici ASCII.
  */
-typedef int (*mr_emit_pair_t)(
-    const char *token,
-    const void *value,
-    size_t      value_size,
-    void       *emit_arg
-);
+typedef int (*mr_emit_pair_t)(const char *token, const void *value, size_t value_size,
+                              void *emit_arg);
 
 /*
  * Funzione usata dal reducer per emettere un risultato finale.
  */
-typedef int (*mr_emit_result_t)(
-    const char *token,
-    const void *result,
-    size_t      result_size,
-    void       *emit_arg
-);
+typedef int (*mr_emit_result_t)(const char *token, const void *result, size_t result_size,
+                                void *emit_arg);
 
 /*
  * Funzione mapper fornita dal programma utente.
  */
-typedef int (*mr_mapper_t)(
-    const mr_file_line_t *line,
-    mr_emit_pair_t        emit,
-    void                 *emit_arg,
-    void                 *user_arg
-);
+typedef int (*mr_mapper_t)(const mr_file_line_t *line, mr_emit_pair_t emit, void *emit_arg,
+                           void *user_arg);
 
 /*
  * Funzione reducer fornita dal programma utente.
  */
-typedef int (*mr_reducer_t)(
-    const char        *token,
-    const mr_value_t  *values,
-    size_t             values_count,
-    mr_emit_result_t   emit,
-    void              *emit_arg,
-    void              *user_arg
-);
+typedef int (*mr_reducer_t)(const char *token, const mr_value_t *values, size_t values_count,
+                            mr_emit_result_t emit, void *emit_arg, void *user_arg);
 
 int mr_attr_init(mr_attr_t *attr);
 int mr_attr_destroy(mr_attr_t *attr);
@@ -94,13 +76,8 @@ int mr_attr_set_reducer_threads(mr_attr_t *attr, size_t n);
 int mr_attr_set_queue_size(mr_attr_t *attr, size_t n);
 int mr_attr_set_log_file(mr_attr_t *attr, const char *path);
 
-int mr_create(
-    mr_t            *mr,
-    const mr_attr_t *attr,
-    mr_mapper_t      mapper,
-    mr_reducer_t     reducer,
-    void            *user_arg
-);
+int mr_create(mr_t *mr, const mr_attr_t *attr, mr_mapper_t mapper, mr_reducer_t reducer,
+              void *user_arg);
 
 int mr_start(mr_t mr, const char *input_path, const char *output_path);
 int mr_destroy(mr_t mr);
