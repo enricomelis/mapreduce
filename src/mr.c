ad74df990e1108b5d77b60097b620167a7b1700d
Enrico <enrico.melis.casa@gmail.com>
2026-05-29 15:54:23 +0200

    defined mr and mr_line_item struct

--- src/mr.c
+++ src/mr.c
@@ -1,5 +1,19 @@
 #include "mr.h"
+#include <errno.h>
 
-/*
- * Scheletro iniziale del modulo della libreria.
- */
+/* stato interno del framework */
+struct mr {
+    mr_attr_t attr;
+    mr_mapper_t mapper;
+    mr_reducer_t reducer;
+    void *user_arg;
+};
+
+/* container di righe */
+typedef struct {
+    char *file_name;
+    size_t file_name_len;
+    unsigned long line_numer;
+    char *line;
+    size_t line_len;
+} mr_line_item_t;
